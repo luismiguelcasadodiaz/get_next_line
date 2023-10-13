5eee7ed23c720ba2303cf13d05c7e011fcf0c542
Luis Miguel Casado Diaz <luismiguelcasadodiaz@gmail.com>
2023-10-13 11:09:42 +0200

    initial setup

---
+++ tests/touch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ vim -c "Stdheader" -c wq get_next_line.c
+ vim -c "Stdheader" -c wq get_next_line.h
+ vim -c "Stdheader" -c wq get_next_line_bonus.c
+ vim -c "Stdheader" -c wq get_next_line_bonus.h
+ vim -c "Stdheader" -c wq get_next_line_utils.c
+ vim -c "Stdheader" -c wq get_next_line_utils_bonus.c
